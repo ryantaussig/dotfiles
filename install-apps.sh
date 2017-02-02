51c27aed71aaf368bb88177e0f7b1fc27cbeee44
Ryan Taussig <ryantaussig@gmail.com>
2017-02-02 11:05:54 -0500

    added sphinx and additional ham packages

--- install-apps.sh
+++ install-apps.sh
@@ -2,16 +2,20 @@
 
 ########
 # author: ryantaussig
-# last update: 2017-01-25
+# last update: 2017-02-02
 # license: GPLv2.0
 #
 # abstract:
 #   the following script installs basic applications and corresponding documentation.
 ########
 
+# install vim
+
+sudo apt-get install vim-nox vim-doc
+
 # install gvim
 
-sudo apt-get install vim-gnome vim-doc
+sudo apt-get install vim-gnome
 
 # install git (should already be installed if this script was obtained from github)
 
@@ -24,6 +28,10 @@ sudo apt-get install ssh
 # install asciidoctor
 
 sudo apt-get install asciidoctor asciidoctor-doc
+
+# install sphinx, related packages, and documentation
+
+sudo apt-get install python-sphinx python3-sphinx python-doc8 python3-doc8  python-numpydoc python3-numpydoc python-doc8-doc sphinx-doc
 
 # install libreoffice metapackage
 
@@ -41,21 +49,21 @@ sudo apt-get install python-numpy python-scipy python-matplotlib ipython ipython
 
 sudo apt-get install python3-numpy python3-scipy python3-matplotlib ipython3 ipython3-notebook python3-pandas python3-sympy python3-nose2
 
-# install available documentation for scipy stack
+# install documentation for scipy stack
 
-sudo apt-get install python-numpy-doc python-scipy-doc python-matplotlib-doc ipython-doc python-pandas-doc python-sympy-doc python-nose2-doc
+sudo apt-get install python-doc python-examples python3-doc python3-examples python-numpy-doc python-scipy-doc python-matplotlib-doc ipython-doc python-pandas-doc python-sympy-doc python-nose2-doc 
 
-# install supplements to scipy stack
+# install supplements to scipy stack with documentation
 
-sudo apt-get install python-pyfftw python3-pyfftw cython cython3 cython-doc
+sudo apt-get install python-pyfftw python3-pyfftw cython cython cython-doc
 
 # install freecad and documentation
 
 sudo apt-get install freecad freecad-doc freecad-dev
 
-# install amateur radio applications
+# install amateur radio applications with available documentation
 
-sudo apt-get install chirp hamradio* ax25* gnuradio*
+sudo apt-get install chirp hamradio* ax25-apps ax25-tools ax25-xtools ax25mail-utils gnuradio gnuradio-dev gnuradio-doc direwolf direwolf-docs
 
 # install youtube-dl
 
