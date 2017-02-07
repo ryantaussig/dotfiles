eaa698fd70d900cd7b1bd8baccba54399f398d1e
Ryan Taussig <ryantaussig@gmail.com>
2017-02-06 19:03:27 -0500

    added ipython, updated symlink script, removed jackdrc

--- make-symlinks.sh
+++ make-symlinks.sh
@@ -14,7 +14,7 @@
 
 dir=~/dotfiles                    # dotfiles directory
 olddir=~/dotfiles_old             # old dotfiles backup directory
-files="bashrc vimrc vim jackdrc"    # list of files/folders to symlink in homedir
+files="bashrc vimrc vim ipython"    # list of files/folders to symlink in homedir
 
 ##########
 
