abeb820df022478a6c9959b47705fbe60e6ae625
zig <zig@c720.attlocal.net>
2017-10-13 18:26:47 -0700

    changes for switch to fedora

--- make-symlinks.sh
+++ make-symlinks.sh
@@ -14,7 +14,7 @@
 
 dir=~/dotfiles                    # dotfiles directory
 olddir=~/dotfiles_old             # old dotfiles backup directory
-files="bashrc vimrc vim ipython Xresources"    # list of files/folders to symlink in homedir
+files="bashrc vimrc vim Xresources"    # list of files/folders to symlink in homedir
 
 ##########
 
